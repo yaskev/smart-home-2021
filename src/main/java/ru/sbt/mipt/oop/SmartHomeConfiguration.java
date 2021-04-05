c636f33699eb8c64fcb40a2343f9f287c7eae6ae
Alexander Yaskevich <yaskevich.san@mail.ru>
2021-04-05 20:18:25 +0300

    Make improvements

--- src/main/java/ru/sbt/mipt/oop/SmartHomeConfiguration.java
+++ src/main/java/ru/sbt/mipt/oop/SmartHomeConfiguration.java
@@ -4,17 +4,31 @@ import com.coolcompany.smarthome.events.SensorEventsManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.sbt.mipt.oop.adapters.CCEventHandlerAdapter;
+import ru.sbt.mipt.oop.events.*;
 import ru.sbt.mipt.oop.handlers.DoorEventHandler;
 import ru.sbt.mipt.oop.handlers.*;
+import ru.sbt.mipt.oop.notifiers.Notifier;
+import ru.sbt.mipt.oop.notifiers.SMSNotifier;
+import ru.sbt.mipt.oop.signalling.Alarm;
+import ru.sbt.mipt.oop.wrappers.AlarmWrappedHandler;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 
 @Configuration
 public class SmartHomeConfiguration {
     @Bean
     SensorEventsManager sensorEventsManager() {
+        HashMap<String, String> eventClasses = new HashMap<>();
+        eventClasses.put("LightIsOn", LightOnEvent.class.getCanonicalName());
+        eventClasses.put("LightIsOff", LightOffEvent.class.getCanonicalName());
+        eventClasses.put("DoorIsOpen", DoorOpenedEvent.class.getCanonicalName());
+        eventClasses.put("DoorIsClosed", DoorClosedEvent.class.getCanonicalName());
+
         SensorEventsManager manager = new SensorEventsManager();
-        manager.registerEventHandler(new CCEventHandlerAdapter(doorEventHandler));
-        manager.registerEventHandler(new CCEventHandlerAdapter(hallDoorEventHandler));
-        manager.registerEventHandler(new CCEventHandlerAdapter(lightEventHandler));
+        manager.registerEventHandler(new CCEventHandlerAdapter(alarmWrappedHandler(alarm(), Arrays.asList(doorEventHandler(smartHome()),
+                hallDoorEventHandler(smartHome(), sender()), lightEventHandler(smartHome())), notifier()), eventClasses));
 
         return manager;
     }
@@ -42,6 +56,21 @@ public class SmartHomeConfiguration {
     @Bean
     LightEventHandler lightEventHandler(SmartHome smartHome) {
         return new LightEventHandler(smartHome);
+    }
+
+    @Bean
+    AlarmWrappedHandler alarmWrappedHandler(Alarm alarm, Collection<EventHandler> handlers, Notifier notifier) {
+        return new AlarmWrappedHandler(alarm, handlers, notifier);
+    }
+
+    @Bean
+    Alarm alarm() {
+        return new Alarm();
+    }
+
+    @Bean
+    Notifier notifier() {
+        return new SMSNotifier();
     }
 
 }
